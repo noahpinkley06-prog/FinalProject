b411714cfdf57f3e6e214094d21bb75ff61492f3
KacieInscoe <kacie.inscoe@bsu.edu>
2025-10-16 13:09:01 -0400

    Update AddingGame.java

--- src/main/java/edu/bsu/cs/AddingGame.java
+++ src/main/java/edu/bsu/cs/AddingGame.java
@@ -1,4 +1,29 @@
 package edu.bsu.cs;
 
+package edu.bsu.cs;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+import java.util.Random;
+
 public class AddingGame {
-}
+
+    private final Random random = new Random();
+    private int score = 0;
+
+    public void start(Stage stage, Font font, Scene mainMenuScene) {
+        VBox root = new VBox(25);
+        root.setAlignment(Pos.TOP_CENTER);
+        root.setStyle("-fx-background-color: #f6e58d; -fx-padding: 40;");
+
+        // Title
+        Label title = new Label("🎲 Adding Game 🎲");
+        title.setStyle(
+                "-fx-font-family: '" + font.getFamily() + "';" +
+                        "-fx-font-size: 60px;" +
+                        "-fx-font-weight: bold;"
+        );
